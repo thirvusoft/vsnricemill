9e3bce24e2e0f03732f40bf69ceeb6066f2e5c0d
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2023-03-24 13:31:22 +0530

    POS Awesome Change

--- vsnricemill/vsnricemill/custom/py/sales_invoice.py
+++ vsnricemill/vsnricemill/custom/py/sales_invoice.py
@@ -2,7 +2,6 @@ import json
 import frappe
 from frappe.model.naming import make_autoname
 
-
 def auto_name(doc, actions):
     if(doc.is_pos==1):
         doc.name = make_autoname(doc.pos_series,doc=doc)
@@ -10,6 +9,10 @@ def auto_name(doc, actions):
 @frappe.whitelist()
 def get_attribute(items):
     item = json.loads(items)
-    for i in item:
-        get_abb = frappe.db.get_value("Item Variant Attribute",{"parent":i.get('item_code'),"attribute":"Size"},"attribute_value")
+    # for i in item:
+    get_abb = frappe.db.get_value("Item Variant Attribute",{"parent":item.get('item_code'),"attribute":"Size"},"attribute_value")
     return get_abb
+
+def validate(doc,event):
+    for i in doc.items:
+        i.size = frappe.db.get_value("Item Variant Attribute",{"parent":i.item_code,"attribute":"Size"},"attribute_value")
