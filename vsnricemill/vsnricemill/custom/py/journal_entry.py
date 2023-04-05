cb43ce90840013366fd81eaf121dc0142a3d5548
Ubuntu <ubuntu@ip-172-26-5-59.ap-south-1.compute.internal>
2023-04-05 04:31:30 +0000

    Feat: Custom field Added

--- vsnricemill/vsnricemill/custom/py/journal_entry.py
+++ vsnricemill/vsnricemill/custom/py/journal_entry.py
@@ -5,12 +5,12 @@ from frappe.email.doctype.notification.notification import get_context
 from frappe.integrations.doctype.slack_webhook_url.slack_webhook_url import send_slack_message
 def update(doc,actions):
     if doc.voucher_type == "Journal Entry":
-        bank_account =  frappe.get_list("Account",{'name':"Axis - VSN"},"limit")
+        bank_account =  frappe.get_list("Account",{'name':"SBI - VSN"},"limit")
         print(bank_account[0].limit * 90 /100 - bank_account[0].limit)
         get_balance = bank_account[0].limit - bank_account[0].limit * 90 /100
         for i in doc.accounts:
             
-            if i.account == "Axis - VSN" and i.credit_in_account_currency :
+            if i.account == "SBI - VSN" and i.credit_in_account_currency :
                 bal = get_balance_on(i.account, date = None, cost_center=None)
                 if bal < i.credit_in_account_currency:
                     send_slack_message(
