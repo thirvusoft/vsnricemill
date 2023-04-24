fd552796d180e2b81095e874a0401bc8c0357926
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2023-04-24 10:51:49 +0530

    Fixe the cc account Issue:

--- vsnricemill/vsnricemill/custom/py/journal_entry.py
+++ vsnricemill/vsnricemill/custom/py/journal_entry.py
@@ -5,26 +5,28 @@ from frappe.email.doctype.notification.notification import get_context
 from frappe.integrations.doctype.slack_webhook_url.slack_webhook_url import send_slack_message
 def update(doc,actions):
     if doc.voucher_type == "Journal Entry":
-        bank_account =  frappe.get_list("Account",{'name':"Axis - VSN"},"limit")
-        print(bank_account[0].limit * 90 /100 - bank_account[0].limit)
-        get_balance = bank_account[0].limit - bank_account[0].limit * 90 /100
+        # for i in doc.accounts:
+        #     bank_account =  frappe.get_list("Account",{'name':i.account},"limit")
+        #     get_balance = bank_account[0].limit - bank_account[0].limit * 90 /100
         for i in doc.accounts:
-            
-            if i.account == "Axis - VSN" and i.credit_in_account_currency :
-                bal = get_balance_on(i.account, date = None, cost_center=None)
-                if bal < i.credit_in_account_currency:
-                    send_slack_message(
-                        webhook_url="Notify",
-                        message=f'Your available balance is {bal} but you have initiated payment for {i.credit_in_account_currency}',#frappe.render_template(doc.name, get_context(doc)),
-                        reference_doctype=doc.doctype,
-                        reference_name=doc.name,
-                    )
-                    frappe.msgprint(f'Your available balance is {bal} but you have initiated payment for {i.credit_in_account_currency}')
-                elif bal  < get_balance:
-                    send_slack_message(
-                        webhook_url="Notify",
-                        message=f"You have almost exhasuted your CC {i.account} limit",#frappe.render_template(doc.name, get_context(doc)),
-                        reference_doctype=doc.doctype,
-                        reference_name=doc.name,
-                    )
-                    frappe.msgprint(f"You have almost exhasuted your CC {i.account} limit")
+            bank_account =  frappe.get_doc("Account",i.account)
+            if bank_account.cc_account == 1:
+                get_balance = bank_account.limit - bank_account.limit * 90 /100
+                if i.credit_in_account_currency :
+                    bal = get_balance_on(i.account, date = None, cost_center=None)
+                    if bal < i.credit_in_account_currency:
+                        send_slack_message(
+                            webhook_url="Notify",
+                            message=f'Your available balance is {bal} but you have initiated payment for {i.credit_in_account_currency}',#frappe.render_template(doc.name, get_context(doc)),
+                            reference_doctype=doc.doctype,
+                            reference_name=doc.name,
+                        )
+                        frappe.msgprint(f'Your available balance is {bal} but you have initiated payment for {i.credit_in_account_currency}')
+                    elif bal  < get_balance:
+                        send_slack_message(
+                            webhook_url="Notify",
+                            message=f"You have almost exhasuted your CC {i.account} limit",#frappe.render_template(doc.name, get_context(doc)),
+                            reference_doctype=doc.doctype,
+                            reference_name=doc.name,
+                        )
+                        frappe.msgprint(f"You have almost exhasuted your CC {i.account} limit")
--- vsnricemill/vsnricemill/custom/py/payment_entry.py
+++ vsnricemill/vsnricemill/custom/py/payment_entry.py
@@ -5,23 +5,23 @@ from frappe.email.doctype.notification.notification import get_context
 from frappe.integrations.doctype.slack_webhook_url.slack_webhook_url import send_slack_message
 def update(doc,actions):
     if doc.payment_type == "Pay":
-        bank_account =  frappe.get_list("Account",{'name':doc.paid_from},"limit")
-        print(bank_account[0].limit - bank_account[0].limit * 90 /100  )
-        get_balance = bank_account[0].limit - bank_account[0].limit * 90 /100 
-        bal = get_balance_on(doc.paid_from, date = None, cost_center=None)
-        if bal < doc.paid_amount:
-            send_slack_message(
-                webhook_url="Notify",
-                message=f'Your available balance is {bal} but you have initiated payment for {doc.paid_amount}',#frappe.render_template(doc.name, get_context(doc)),
-                reference_doctype=doc.doctype,
-                reference_name=doc.name,
-            )
-            frappe.msgprint(f'Your available balance is {bal} but you have initiated payment for {doc.paid_amount}')
-        elif bal  < get_balance:
-            send_slack_message(
-                webhook_url="Notify",
-                message=f"You have almost exhasuted your CC {doc.paid_from} limit",#frappe.render_template(doc.name, get_context(doc)),
-                reference_doctype=doc.doctype,
-                reference_name=doc.name,
-            )
-            frappe.msgprint(f"You have almost exhasuted your CC {doc.paid_from} limit")
+        bank_account =  frappe.get_doc("Account",doc.paid_from)
+        if bank_account.cc_account == 1:
+            get_balance = bank_account.limit - bank_account.limit * 90 /100 
+            bal = get_balance_on(doc.paid_from, date = None, cost_center=None)
+            if bal < doc.paid_amount:
+                send_slack_message(
+                    webhook_url="Notify",
+                    message=f'Your available balance is {bal} but you have initiated payment for {doc.paid_amount}',#frappe.render_template(doc.name, get_context(doc)),
+                    reference_doctype=doc.doctype,
+                    reference_name=doc.name,
+                )
+                frappe.msgprint(f'Your available balance is {bal} but you have initiated payment for {doc.paid_amount}')
+            elif bal  < get_balance:
+                send_slack_message(
+                    webhook_url="Notify",
+                    message=f"You have almost exhasuted your CC {doc.paid_from} limit",#frappe.render_template(doc.name, get_context(doc)),
+                    reference_doctype=doc.doctype,
+                    reference_name=doc.name,
+                )
+                frappe.msgprint(f"You have almost exhasuted your CC {doc.paid_from} limit")
