6aceb9abf1dfe35826bd16e660320848ef6cef9d
Ubuntu <ubuntu@ip-172-26-4-126.ap-south-1.compute.internal>
2023-05-12 07:02:55 +0000

    Naming Series

--- vsnricemill/vsnricemill/custom/py/sales_invoice.py
+++ vsnricemill/vsnricemill/custom/py/sales_invoice.py
@@ -12,6 +12,8 @@ from frappe import _, msgprint, throw
 def auto_name(doc, actions):
     if(doc.pos_profile == "CounterSales VSN"):
         doc.name = make_autoname(f"{doc.name_series}.-.{doc.pos_series}.-.#####",doc=doc)
+    elif(doc.pos_profile == "VSN Bill 1"):
+        doc.name = make_autoname(f"{doc.name_series}.-.{doc.pos_series}.-.#####",doc=doc)
 def counter_sales(doc, actions):
     if (doc.pos_profile == "JM Shop"):
       doc.name = make_autoname(f"{doc.c_sales}.-.{doc.pos_series}.-.#####",doc=doc)
