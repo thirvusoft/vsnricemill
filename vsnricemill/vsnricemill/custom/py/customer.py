3b18ae2b32f8fc61723f93a850559eb8c2107a5a
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2023-05-24 12:41:10 +0530

    Feat: Customer Phone Number Validation

--- vsnricemill/vsnricemill/custom/py/customer.py
+++ vsnricemill/vsnricemill/custom/py/customer.py
@@ -5,8 +5,6 @@ def validate_phone_number(doc, actions):
     phone_pattern = r'^(\+91[-\s]?)?[0]?(91)?[789]\d{9}$'
     if re.match(phone_pattern, doc.mobile_no):
         frappe.msgprint("Phone number is valid.")
-        return True
     else:
         frappe.throw("Phone number is not valid.")
-        return False
 
